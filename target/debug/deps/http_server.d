9144a6e7c70c6860fcbbf94ebc4229b61dd4fee2
David Kaestle <davidsimonkastle@gmail.com>
2021-03-03 21:51:19 -0300

    adding .gitignore

--- target/debug/deps/http_server.d
+++
@@ -1,9 +0,0 @@
-C:\Users\david\dev\rust-udemy-course\http-server\target\debug\deps\http_server.exe: src\main.rs src\http\mod.rs src\http\method.rs src\http\request.rs src\server.rs
-
-C:\Users\david\dev\rust-udemy-course\http-server\target\debug\deps\http_server.d: src\main.rs src\http\mod.rs src\http\method.rs src\http\request.rs src\server.rs
-
-src\main.rs:
-src\http\mod.rs:
-src\http\method.rs:
-src\http\request.rs:
-src\server.rs:
--- target/rls/debug/deps/http_server-e955a84c46749c50.d
+++
@@ -1,9 +0,0 @@
-c:\Users\david\dev\rust-udemy-course\http-server\target\rls\debug\deps\http_server-e955a84c46749c50.rmeta: src\main.rs src\http\mod.rs src\http\method.rs src\http\request.rs src\server.rs
-
-c:\Users\david\dev\rust-udemy-course\http-server\target\rls\debug\deps\http_server-e955a84c46749c50.d: src\main.rs src\http\mod.rs src\http\method.rs src\http\request.rs src\server.rs
-
-src\main.rs:
-src\http\mod.rs:
-src\http\method.rs:
-src\http\request.rs:
-src\server.rs:
